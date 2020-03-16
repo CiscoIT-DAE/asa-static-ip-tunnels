b3ca2a8ae1d4b509cd990d2cb92030e3df2c4f62
Drew Taylor <drewtaylor888@gmail.com>
2020-03-16 16:58:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# ASA Static IP Tunnels
+
+*Static IP Tunnel Configuration Generator for ASAs and AnyConnect VPN*
+
+This repo contains helper functions designed to generate static IP tunnel configurations for the ASA CLI.
+
+From a high level:
+```
+  VPN Client "a"             VPN Client "b"
+         |                          |
+         v                          v
+https://<vpn-device>/a     https://<vpn-device>/b
+         |                          |
+         v                          v
+   tunnel-group a ----------- tunnel-group b ---> shared group-policy
+         |                          |          -> shared DAP                                
+         v                          v
+  ip pool a  ->  10.0.0.1    ip local b -> 10.0.0.2
+         |                          |
+         v                          v
+     Connected!                 Connected!
+```
+
+Please take note of the DAP LUA configuration that goes along with this:
+```
+EVAL(cisco.aaa.username, "EQ", cisco.aaa.tunnelgroup)
+```
+This is a security necessity to ensure static IPs are taken by the users intended.
+
+The original intent for this program was to satisfy India's VoIP exception for users with static IPs in light of recent events due to COVID-19.
+
+## Installation
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## To Run
+```
+python asa_static_ip_tunnels.py
+```
+
+## Technologies & Frameworks Used
+
+**Cisco Products & Services:**
+
+- ASA OS Software
+- AnyConnect VPN Client
+
+**Tools & Frameworks:**
+
+- Python 3.7
+- `ipaddress` module
+
+## File Structure
+```
+.
+├── asa_static_ip_tunnels.py (where __main__ lies and a code explanation on how to use functions)
+├── users.txt (input file for user IDs)
+├── static_tunnels.py (configuration generation functions)
+├── output (configuration output)
+|   ├── config.txt (creation)
+|   └── clear_config.txt (deletion)
+```
+
+## Authors & Maintainers
+
+Smart people responsible for the creation and maintenance of this project:
+
+- Drew Taylor <dretaylo@cisco.com>
+
+## Credits
+
+Engineers: Damien Stenning (dstennin@cisco.com), Nishant Singh (nishansi@cisco.com), Pete Davis (psd@cisco.com), Fernando De Jesus Sancho Vargas (fsanchov@cisco.com).
+
+## License
+
+This project is licensed to you under the terms of the [Cisco Sample
+Code License](./LICENSE).
---
+++ static_tunnels.py
@@ -0,0 +1,104 @@
+"""ASA Static IP Tunnels Console Script.
+
+Copyright (c) 2020 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Drew Taylor"
+__email__ = "dretaylo@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2020 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+import ipaddress
+
+def generateStaticTunnelsConfig(users, device, ip_start, aaa_server, group_policy):
+    """
+        Generates ASA configuration for static IP allocation to users.
+        This creates a unique address pool and tunnel group for each user.
+
+        :param users: username ids that need static IPs
+        :type users: list
+        :param device: hostname of device
+        :type device: str
+        :param ip_start: first IP address in address pool
+        :type ip_start: str
+        :param aaa_server: name of authentication server for tunnel group
+        :type aaa_server: str
+        :param group_policy: name of group policy to attach tunnel groups to
+        :type group_policy: str
+
+        :return: configuration for the ASA
+        :rtype: str
+    """
+
+    config = ""
+    ip = ipaddress.ip_address(ip_start)
+
+    for user in users:
+        # ip local pool <USER> X.X.X.X mask 255.255.255.255
+        config += f"ip local pool {user} {str(ip)} mask 255.255.255.255\n"
+
+        # tunnel-group <USER> type remote-access
+        config += f"tunnel-group {user} type remote-access\n"
+
+        # tunnel-group <USER> general-attributes
+        config += f"tunnel-group {user} general-attributes\n"
+
+        # address-pool <USER>
+        config += f"address-pool {user}\n"
+
+        # authentication-server-group <AAA_SERVER>
+        config += f"authentication-server-group {aaa_server}\n"
+
+        # default-group-policy <GROUP-POLICY>
+        config += f"default-group-policy {group_policy}\n"
+
+        # tunnel-group <USER> webvpn-attributes
+        config += f"tunnel-group {user} webvpn-attributes\n"
+
+        # group-url https://<DEVICE>.cisco.com/<USER> enable
+        config += f"group-url https://{device}.cisco.com/{user} enable\n"
+
+        config += "\n"
+        ip += 1
+
+    return config
+
+def clearStaticTunnelsConfig(users):
+    """
+        Generates ASA configuration to clear static IP allocation for users.
+
+        :param users: username ids that have unique address pools and tunnel groups
+        :type users: list
+
+        :return: configuration for the ASA
+        :rtype: str
+    """
+
+    config = ""
+
+    for user in users:
+        # clear configure tunnel-group <USER>
+        config += f"clear configure tunnel-group {user}\n"
+
+        # no ip local pool <USER>
+        config += f"no ip local pool {user}\n"
+
+        config += "\n"
+
+    return config
